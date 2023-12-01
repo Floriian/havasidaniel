b3d254840b3468d3b7e423695b557a078af4dbab
Floriian <lovasifloriian@outlook.hu>
2023-12-01 20:54:56 +0100

    add basic email.

--- src/features/contact/actions/createContact.action.ts
+++ src/features/contact/actions/createContact.action.ts
@@ -1,9 +1,17 @@
 "use server";
 
 import { ContactSchema } from "@/features/contact";
+import { transporter } from "@features/contact/mailer";
 
 export async function createContact(formData: ContactSchema) {
-  console.log({ formData });
+  const result = await transporter.sendMail({
+    from: "havasidaniel.hu",
+    to: "lovasifloriian@outlook.hu",
+    subject: `Kapcsolat - ${formData.name}`,
+    html: `${JSON.stringify(formData)}`,
+  });
+
+  if (!result) return { success: false };
   return {
     success: true,
   };
---
+++ src/features/contact/mailer/mailer.ts
@@ -0,0 +1,10 @@
+import { createTransport } from "nodemailer";
+
+export const transporter = createTransport({
+  host: process.env.EMAIL_HOST,
+  port: +process.env.EMAIL_PORT!,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
