fb2e7df5d7a084c0072ce7dd919f7291ac9fa360
Floriian <lovasifloriian@outlook.hu>
2023-12-11 21:04:20 +0100

    update env example.

--- src/features/aws/s3.ts
+++ src/features/aws/s3.ts
@@ -1,8 +1,8 @@
 import { S3Client as AWSS3Client } from "@aws-sdk/client-s3";
 export const S3Client = new AWSS3Client({
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
-    secretAccessKey: process.env.AWS_SECRET!,
+    accessKeyId: process.env.HED_AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.HED_AWS_SECRET!,
   },
   region: "eu-central-1",
 });
