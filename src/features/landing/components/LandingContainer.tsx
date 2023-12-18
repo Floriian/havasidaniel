0ea83d4e36aa9dfa318b5d7dc9bc2857aab9b8ef
Floriian <lovasifloriian@outlook.hu>
2023-12-18 22:07:11 +0100

    fix: landing container positioning

--- src/features/landing/components/LandingContainer.tsx
+++ src/features/landing/components/LandingContainer.tsx
@@ -16,9 +16,9 @@ export function LandingContainer() {
           width={1920}
           className="absolute w-9/12 -translate-x-1/2 left-1/2"
         />
-        <div className='relative h-1/2 w-full'>
+        <div className='relative h-auto w-full flex flex-col items-center'>
           <div className="flex flex-col items-center justify-center w-full">
-            <Image src="/logo.webp" alt="Havasi Dániel Edvin" priority={true} height={768} width={768} />
+            <Image src="/logo.webp" alt="Havasi Dániel Edvin" priority={true} height={1024} width={1024} />
             <div className="flex gap-4">
               <Button>Lorem</Button>
               <Button>Ipsum</Button>
@@ -28,7 +28,7 @@ export function LandingContainer() {
                 <ArrowDownward />
               </Link>
             </div>
-            <p className="max-w-3/4 w-3/4">
+            <p className="max-w-3/4 w-3/4 mt-10">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam eos aut maxime veniam harum dicta explicabo consequuntur pariatur reiciendis hic id voluptatem sapiente ab, blanditiis deserunt facilis itaque. Pariatur, excepturi!
             </p>
           </div>
