8bc3ca08a6486d666132851c2a2abce89a067d96
Floriian <lovasifloriian@outlook.hu>
2023-12-18 17:15:01 +0100

    other: npm-pnpm.

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,31 +2,50 @@
 import { useAppStore } from "@features/app";
 import clsx from "clsx";
 import Link from "next/link";
-import InstagramIcon from '@mui/icons-material/Instagram';
-import {ardela} from "@/utils";
-
-
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { ardela } from "@/utils";
 
 export function Navbar() {
-    const { isInLanding, activeArticle } = useAppStore();
+  const { isInLanding, activeArticle } = useAppStore();
 
-    const backgroundColor = clsx("flex justify-between absolute p-4 w-full transition-colors duration-150 bg-transparent", {
-        "bg-zinc-950": !isInLanding,
-    })
+  const backgroundColor = clsx(
+    "flex justify-between absolute p-4 w-full transition-colors duration-150 bg-transparent ",
+    {
+      "bg-zinc-950": !isInLanding,
+    },
+  );
 
-    const activeLink = clsx("text-[14px] py-2.5 px-1.5 border-2 rounded-md bg hover:bg-white hover:text-purple-900 transition-colors duration-300 backdrop-blur-xl")
+  const activeLink = clsx(
+    "text-[14px] py-2.5 px-1.5 border-2 rounded-md hover:bg-white hover:text-purple-900 transition-colors duration-300 backdrop-blur-xl",
+  );
 
-    return (
-        <header className="sticky z-50 top-0 ">
-            <nav className={backgroundColor}>
-                <div><h3 className={"uppercase text-[20px] " + ardela.className}>HED Photography</h3></div>
-                <div className="flex gap-[10px] uppercase">
-                    <Link href="#landing" scroll={true} className={activeLink}>Bemutatkozás</Link>
-                    <Link href="#gallery" scroll={true} className={activeLink}>Galéria</Link>
-                    <Link href="#contact" scroll={true} className={activeLink}>Kapcsolat</Link>
-                </div>
-                <a href={"https://instagram.com"} target="_blank" className={activeLink}><InstagramIcon/> <span>@havasidaniel</span></a>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="sticky z-50 top-0">
+      <nav className={backgroundColor}>
+        <div>
+          <h3 className={"uppercase text-[20px] " + ardela.className}>
+            HED Photography
+          </h3>
+        </div>
+        <div className="flex gap-[10px] uppercase">
+          <Link href="#landing" scroll={true} className={activeLink}>
+            Bemutatkozás
+          </Link>
+          <Link href="#gallery" scroll={true} className={activeLink}>
+            Galéria
+          </Link>
+          <Link href="#contact" scroll={true} className={activeLink}>
+            Kapcsolat
+          </Link>
+        </div>
+        <a
+          href={"https://instagram.com"}
+          target="_blank"
+          className={activeLink}
+        >
+          <InstagramIcon /> <span>@havasidaniel</span>
+        </a>
+      </nav>
+    </header>
+  );
 }
--- src/features/landing/components/LandingContainer.tsx
+++ src/features/landing/components/LandingContainer.tsx
@@ -1,27 +1,34 @@
 import Image from "next/image";
 import { ardela } from "@/utils";
 export function LandingContainer() {
-    return (
-        <div className="relative w-full">
-            <Image
-                src="/header.png"
-                alt="Havasi Dániel Edvin"
-                priority={true}
-                height={1293}
-                width={1920}
-            />
-            <div
-                className="absolute text-2xl md:text-7xl text-white top-96 left-1/2 -translate-x-1/2 -translate-y-1/2 m-auto">
-                <div className="relative">
-                    <Image src="/logo.webp" width={1024} height={1024} alt="Havasi Dániel Edvin" />
-                </div>
-                <h1 className={ardela.className + " text-center"}>HED</h1>
-                <h1 className={ardela.className + " text-center"}>PHOTOGRAPHY</h1>
-
-                <div className="absolute text-xl w-full">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam tempore, velit magni voluptate nihil iste tenetur! Atque rem ipsum cum quae doloremque! Qui tempora molestiae fuga odit et, aliquid temporibus!
-                </div>
-            </div>
+  return (
+    <div className="relative w-full">
+      <Image
+        src="/header.png"
+        alt="Havasi Dániel Edvin"
+        priority={true}
+        height={1293}
+        width={1920}
+      />
+      <div className="absolute text-2xl md:text-7xl text-white top-96 left-1/2 -translate-x-1/2 -translate-y-1/2 m-auto ">
+        <div className="relative">
+          <Image
+            src="/logo.webp"
+            width={1024}
+            height={1024}
+            alt="Havasi Dániel Edvin"
+          />
         </div>
-    )
+        <h1 className={ardela.className + " text-center"}>HED</h1>
+        <h1 className={ardela.className + " text-center"}>PHOTOGRAPHY</h1>
+
+        <div className="absolute text-xl w-full">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam
+          tempore, velit magni voluptate nihil iste tenetur! Atque rem ipsum cum
+          quae doloremque! Qui tempora molestiae fuga odit et, aliquid
+          temporibus!
+        </div>
+      </div>
+    </div>
+  );
 }
