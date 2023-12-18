26389d9f3ce5fec6f5da64f4685b3b2c7325c60d
Floriian <lovasifloriian@outlook.hu>
2023-12-18 23:49:40 +0100

    feat: toggle drivider on article component

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,20 +1,27 @@
 import { cn } from "@utils/cn";
+import clsx from "clsx";
 import { forwardRef, HTMLAttributes } from "react";
 
 export interface ArticleProps extends HTMLAttributes<HTMLElement> {
     children: React.ReactNode;
+    toggleDivider?: boolean;
 }
 
 export const Article = forwardRef<HTMLElement, ArticleProps>(
-    ({ className, children, ...props }, ref) => {
+    ({ className, children, toggleDivider, ...props }, ref) => {
+
+        const dividerClasses = clsx('h-px w-[100vw-5rem] border-gray-600 border mx-24 my-8', {
+            'hidden': toggleDivider,
+        })
+
         return (
             <>
-                <hr className="h-px w-[100vw-5rem] border-gray-600 border mx-24 my-8" />
                 <article
                     ref={ref}
                     className={cn("sm:px-2 md:px-24", className)}
                     {...props}
                 >{children}</article>
+                <hr className={dividerClasses} />
             </>
         );
     }
