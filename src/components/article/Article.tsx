5e07faebae7c698901bd577f1c86ec5f8900f58b
Floriian <lovasifloriian@outlook.hu>
2023-12-18 23:12:07 +0100

    chore: stricter article

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,16 +1,18 @@
 import { cn } from "@utils/cn";
 import { forwardRef, HTMLAttributes } from "react";
 
-export interface ArticleProps extends HTMLAttributes<HTMLElement> { }
+export interface ArticleProps extends HTMLAttributes<HTMLElement> {
+    children: React.ReactNode;
+}
 
 export const Article = forwardRef<HTMLElement, ArticleProps>(
-    ({ className, ...props }, ref) => {
+    ({ className, children, ...props }, ref) => {
         return (
             <article
                 ref={ref}
                 className={cn("sm:px-2 md:px-24", className)}
                 {...props}
-            />
+            >{children}</article>
         );
     }
 )
