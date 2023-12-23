a5b69dca5e6e5b046bf566559a3b3aa75883a6ae
Floriian <lovasifloriian@outlook.hu>
2023-12-23 16:12:54 +0100

    feat: dynamic buttons on navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,7 +4,6 @@ import clsx from "clsx";
 import Link from "next/link";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import { ardela } from "@/utils";
-import { useEffect } from "react";
 
 export function Navbar() {
   const { isInLanding, activeArticle, setActiveArticle } = useAppStore();
@@ -12,13 +11,16 @@ export function Navbar() {
   const backgroundColor = clsx(
     "hidden md:flex justify-between absolute p-4 w-full transition-colors duration-150 bg-transparent",
     {
-      "bg-zinc-950": !isInLanding,
+      "bg-zinc-950": activeArticle !== "landing",
     },
   );
 
-  const activeLink = clsx(
-    "text-[14px] py-2.5 px-1.5 border-2 rounded-md hover:bg-white hover:text-purple-900 transition-colors duration-300 backdrop-blur-xl",
-  );
+  // const activeLink = "text-[14px] py-2.5 px-1.5 border-2 rounded-md hover:bg-white hover:text-purple-900 transition-colors duration-300 backdrop-blur-xl";
+
+  const isActiveLink = (href: string) => clsx("text-[14px] py-2.5 px-1.5 border-2 rounded-md transition-colors duration-300 backdrop-blur-xl", {
+    "bg-white text-purple-900": activeArticle === href,
+    "bg-transparent text-white": activeArticle !== href,
+  })
 
   return (
     <header className="sticky z-50 top-0">
@@ -29,22 +31,22 @@ export function Navbar() {
           </h3>
         </div>
         <div className="flex gap-[10px] uppercase">
-          <Link href="#landing" scroll={true} className={activeLink} onClick={() => setActiveArticle("landing")}>
+          <Link href="#landing" scroll={true} className={isActiveLink("landing")} onClick={() => setActiveArticle("landing")}>
             Bemutatkozás
           </Link>
-          <Link href="#gallery" scroll={true} className={activeLink} onClick={() => setActiveArticle("gallery")}>
+          <Link href="#gallery" scroll={true} className={isActiveLink("gallery")} onClick={() => setActiveArticle("gallery")}>
             Galéria
           </Link>
-          <Link href="#contact" scroll={true} className={activeLink} onClick={() => setActiveArticle("contacts")}>
+          <Link href="#contact" scroll={true} className={isActiveLink("contacts")} onClick={() => setActiveArticle("contacts")}>
             Kapcsolat
           </Link>
         </div>
         <a
-          href={"https://instagram.com"}
+          href={"https://instagram.com/edvinhavasi_"}
           target="_blank"
-          className={activeLink}
+          className="text-[14px] py-2.5 px-1.5 border-2 rounded-md hover:bg-white hover:text-purple-900 transition-colors duration-300 backdrop-blur-xl"
         >
-          <InstagramIcon /> <span>@havasidaniel</span>
+          <InstagramIcon /> <span>@edvinhavasi_</span>
         </a>
       </nav >
     </header >
