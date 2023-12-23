d0ef863d60563434f300e908f4f70ef8828f57fc
Floriian <lovasifloriian@outlook.hu>
2023-12-23 16:13:37 +0100

    fix: add ref to gallery

--- src/features/gallery/components/GallerySection.tsx
+++ src/features/gallery/components/GallerySection.tsx
@@ -1,9 +1,21 @@
+"use client";
 import { Article } from "@components/article";
+import { useAppStore } from "@features/app";
 import { GalleryImages } from "@features/gallery";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 export function GallerySection() {
+    const { inView, ref } = useInView();
+    const { setActiveArticle } = useAppStore();
+
+    useEffect(() => {
+        if (inView) setActiveArticle("gallery")
+        console.log("GALLERY IN VIEW");
+    }, [inView])
+
     return (
-        <Article id="gallery">
+        <Article id="gallery" ref={ref}>
             <GalleryImages />
         </Article>
     )
