dbe718897f562a766f13f34a706cd7b5c6815d9e
Floriian <lovasifloriian@outlook.hu>
2024-01-01 13:39:29 +0100

    feat: photoswipe.

--- src/features/gallery/components/GalleryImages.tsx
+++ src/features/gallery/components/GalleryImages.tsx
@@ -6,6 +6,9 @@ import { Spinner } from "@components/spinner";
 import { actions, useGalleryStore } from "@features/gallery";
 import { useState, useEffect } from "react";
 import CallReceivedIcon from '@mui/icons-material/CallReceived';
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import 'photoswipe/style.css';
+
 export function GalleryImages() {
     const [gallery, setGallery] = useState<"mixed" | "wedding" | "party">("wedding");
     const [imgs, setImgs] = useState<string[]>();
@@ -22,6 +25,18 @@ export function GalleryImages() {
         fetchImages();
     }, [gallery]);
 
+    useEffect(() => {
+        if (imgs && imgs.length > 0) {
+            let lightBox: PhotoSwipeLightbox | null = new PhotoSwipeLightbox({ gallery: "#gallery", children: "a", pswpModule: () => import("photoswipe"), zoom: false })
+            lightBox.init()
+
+            return () => {
+                lightBox?.destroy();
+                lightBox = null;
+            }
+        }
+    }, [imgs])
+
     return (
         <div className="flex flex-col gap-4 mt-4 mb-4 justify-center">
             <div className="flex justify-center gap-4 ">
@@ -34,10 +49,12 @@ export function GalleryImages() {
                     {gallery === "party" && <CallReceivedIcon />}
                 </Button>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2" id="gallery">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 pswp-gallery" id="gallery">
                 {isLoading && new Array(4).fill(null).map((_, i) => (<Spinner key={i} />))}
                 {!isLoading && imgs?.map((img, i) => (
-                    <Image src={img} alt={img} key={i} sizes="(max-width: 1024px) 100vw, 700px" />
+                    <a href={img} target="_blank" data-pswp-width="1200px" data-pswp-height="800px" key={i} rel="noreferrer">
+                        <Image src={img} alt={img} sizes="(max-width: 1024px) 400px, 600px" />
+                    </a>
                 ))}
             </div>
         </div>
